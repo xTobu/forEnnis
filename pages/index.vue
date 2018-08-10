701b7cd083f78c72505b0dcceee61dff8efd8564
Junxiang <junxiang.oriint@gmail.com>
2018-08-10 20:55:25 +0800

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,8 @@
 <template>
     <section class="container">
-        <div>
-            <app-logo/>
-            <h1 class="title">
-                ennis-cokewithfood
-            </h1>
-            <h2 class="subtitle">
-                Nuxt.js project
-            </h2>
-            <div class="links">
-                <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-                <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-            </div>
-        </div>
+
+        <app-logo/>
+
     </section>
 </template>
 
@@ -24,11 +14,12 @@ export default {
         AppLogo
     },
     mounted() {
-        console.log($)
-    },
+        console.log($);
+    }
 };
 </script>
 
-<style>
+<style lang="scss">
+@import '~assets/sass/index.scss';
 </style>
 
